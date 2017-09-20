44c0175beded1302b61861e57e55f146587469ab
Ioannis Ioakeimidis <jioakim@gmail.com>
2017-09-20 07:06:12 +0300

    Initial Setup
    To run the setup do npm install, npm install -g webpack-dev-server, and run webpack.
    To run the app -> npm run dev

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: "inline-sourcemap",
+  entry: "./jsx/App.jsx",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "app.min.js"
+  }
+};
