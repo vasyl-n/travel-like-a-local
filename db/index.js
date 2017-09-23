f4bba344b637a20c497da9b240217615684921f3
Adam Boettcher <baboettcher@gmail.com>
2017-09-23 15:03:51 -0700

    db exploration

---
+++ db/index.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const bcrypt = require('bcrypt-nodejs');
+const mysqlConfig = require('./config.js');
+
+const connection = mysql.createConnection(mysqlConfig);
+
+var userName = "jay";
+var password = "123";
+var hash = bcrypt.hashSync(password);
+
+const getAllTransactions = function(callback) {
+  connection.query('insert into users (username, password) values ('+ JSON.stringify(userName) +','+ JSON.stringify(hash) +')'), function(err, results){
+    if (err) {
+      console.log(err);
+    } else {
+      //callback(results);
+      console.log("done");
+    }
+  };
+};
+
+getAllTransactions();
