c774a54f6acdc389da490af6caef42307f7f9a6a
Michael Flis <michaelflis07@gmail.com>
2018-05-06 09:36:16 -0700

    Add App.test.js file for testing

---
+++ src/App.test.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+console.log(puppeteer)
+
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,  // define whether Chromium is open and running
+    slowMo: 250,      // slow down operations
+    devtools: true   // open devtools
+  }
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
+}
+
+let browser;
+let page;
+
+beforeAll(async() => {
+  browser = await puppeteer.launch(isDebugging()); // browser instance
+  page = await browser.newPage();  // page instance
+  
+  // tell puppeteer where to navigate to in the browser
+  await page.goto('http://localhost:3000/')
+  
+  // set options for page
+  page.setViewport({width: 500, height: 2400});
+  
+})
+
+describe('on page load', () => {
+  
+  test('h3 loads correctly', async() => {
+    
+    // look for and select an element
+    // const html = await page.$eval('.logo', e => e.innerHTML)
+    
+    // // expect statement / evaluate test
+    // expect(html).toBe('Travel Like a Local
+    expect(2+2).toBe(4);
+            
+  }, 1600) // set timeout for test
+  
+})
+
+afterAll(() => {
+  if ( isDebugging() ) {
+    browser.close()
+  }
+})
