1f64209b930d2250b956bef28171bc84a8464e77
vasyl-n <vasyl.nrk@gmail.com>
2018-05-06 17:07:05 -0700

    styled components

--- src/pages/friends.jsx
+++ src/pages/friends.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import FriendList from "../jsx/FriendList.jsx";
 import AddFriend from '../jsx/AddFriend.jsx';
+import styled from 'styled-components'
 class Friends extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return(
-      <div className="friends">
-        <AddFriend userName={this.props.userName} friendsToAdd={this.props.friendsToAdd} 
-          handleAddFriend={this.props.handleAddFriend} />
+      <FriendsContainer className="friends">
         <FriendList userName={this.props.userName} userID={this.props.userID} 
           friendList={this.props.friendList} handleFriendDelete={this.props.handleFriendDelete} />
-      </div>
+        <AddFriend userName={this.props.userName} friendsToAdd={this.props.friendsToAdd} 
+          handleAddFriend={this.props.handleAddFriend} />
+      </FriendsContainer>
     )
   }
 }
+
+const FriendsContainer = styled.div`
+  display: flex;
+`
+
 
 export default Friends;
