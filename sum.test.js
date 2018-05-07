6ae4b7c94e2f250576e179d1546ecfe432e04968
Michael Flis <michaelflis07@gmail.com>
2018-05-07 11:54:34 -0700

    add script to remove browser environment from node tests

--- sum.test.js
+++ sum.test.js
@@ -4,4 +4,4 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
 })
 
-//console.log(window);
+//console.log(window);  // added "jest": {"testEnvironment": "node"} to package.json  -- should log 'Reference error' undefined during test
