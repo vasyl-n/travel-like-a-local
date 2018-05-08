82323cf7e4317eabe5e28be6d22cb3f856f08eb9
Michael Flis <michaelflis07@gmail.com>
2018-05-07 19:39:17 -0700

    no update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// const React = require('react');
-// const ReactDOM = require('react-dom');
+// const React = require('react')
 // const App = require('./jsx/App.jsx');
 // const enzyme = require('enzyme')
 // var Adapter = require('enzyme-adapter-react-16');
+
 
 // enzyme.configure({ adapter: new Adapter() });
 
@@ -14,3 +14,14 @@
 //   })
   
 // })
+
+
+// // setup file
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-15';
+
+// configure({ adapter: new Adapter() });
+// // test file
+// import { shallow, mount, render } from 'enzyme';
+
+// const wrapper = shallow(<Foo />);
