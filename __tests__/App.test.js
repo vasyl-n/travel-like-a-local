33bec041bb3300a6cf5561e15b01e05fd5bb1c5e
Michael Flis <michaelflis07@gmail.com>
2018-05-07 22:05:32 -0700

    rename tests folder

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react'
-// setup file
 import { configure } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../src/jsx/App.jsx'
 
 configure({ adapter: new Adapter() });
-
-// test file
-
-import { shallow, mount, render } from 'enzyme';
 
 const wrapper = shallow(<App />);
 
