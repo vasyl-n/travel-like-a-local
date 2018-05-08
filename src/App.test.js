0df847f758636a5dc2c73ad1dd23c76ee41bf2d0
Michael Flis <michaelflis07@gmail.com>
2018-05-07 19:00:26 -0700

    add npm i --save-dev enzyme enzyme-adapter-react-16

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+// const App = require('./jsx/App.jsx');
+// const enzyme = require('enzyme')
+// var Adapter = require('enzyme-adapter-react-16');
+
+// enzyme.configure({ adapter: new Adapter() });
+
+// describe('<App />' , () => {
+  
+//   it('renders without crashing', () => {
+//     const wrapper = enzyme.shallow(<App />);
+//     expect(true).toBe(true);
+//   })
+  
+// })
